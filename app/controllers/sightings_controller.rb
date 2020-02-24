2fa0049f8fd0f9c1b2ebf0f5edf5bf3670af3b70
Brandon Weaver <a7xncob@gmail.com>
2020-02-24 23:41:42 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,7 +1,8 @@
 class SightingsController < ApplicationController
   
   def index
-    
+    sightings = Sighting.all
+    render json: SightingSerializer.new(sightings).to_serialized_json
   end
   
   def show
